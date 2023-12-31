6d819937a688e2cd2a936b11bff67d4e8d56b5de
glar3 <keeganbkzn@gmail.com>
2023-12-31 18:12:42 +0200

    Changed placeholders

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatum, voluptas! Corrupti, dolorem ullam reiciendis facilis hic necessitatibus quasi, assumenda explicabo expedita qui dolores veritatis illum officia quos dolor. Exercitationem, sequi.
+document.addEventListener("DOMContentLoaded", function () {
+  const portfolio = document.getElementById("portfolio");
+  const randomQuoteGenerator = document.getElementById("randomQuoteGenerator");
+  const BMI = document.getElementById("BMI");
+  const piano = document.getElementById("piano");
+
+  portfolio.addEventListener("click", function () {
+    window.location = "index.html";
+  });
+  randomQuoteGenerator.addEventListener("click", function () {
+    window.location = "index.html";
+  });
+  BMI.addEventListener("click", function () {
+    window.location = "index.html";
+  });
+  piano.addEventListener("click", function () {
+    window.location = "index.html";
+  });
+});
